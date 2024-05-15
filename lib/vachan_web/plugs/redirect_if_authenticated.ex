474a3e50dead37f78ce27a9f53ff272995bb11de
JanveeBansal <bansaljanvee@gmail.com>
2024-05-15 12:04:56 +0530

    redirect signed in user

--- lib/vachan_web/plugs/redirect_if_authenticated.ex
+++ lib/vachan_web/plugs/redirect_if_authenticated.ex
@@ -1,24 +1,18 @@
 defmodule VachanWeb.Plugs.RedirectIfAuthenticated do
   import Plug.Conn
-  import Phoenix.Controller, only: [put_flash: 3, redirect: 2]
 
   def init(_opts), do: nil
 
-  def call(conn, _) do
-    if is_authenticated(conn) do
+  def call(conn, _opts) do
+    if is_authenticated(conn.assigns[:current_user]) do
       conn
       |> put_flash(:info, "You are already signed in.")
-      |> redirect(to: "/verify-email") # Redirect to the desired page
-      |> halt()
+      |> redirect(to: "/people") # Redirect to the desired page
     else
       conn
     end
   end
 
-  defp is_authenticated(conn) do
-    # Implement your logic to check if the user is authenticated here
-    # For example, you can check if a user is present in the session
-    user_id = get_session(conn, :user_id)
-    !is_nil(user_id)
-  end
+  defp is_authenticated(nil), do: false
+  defp is_authenticated(_user), do: true
 end
