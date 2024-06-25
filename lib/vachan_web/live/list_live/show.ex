8ee2b4240bef8b6ce5efaca62cdefe386dd79649
bind-ritu1211 <kumariritubind@gmail.com>
2024-06-25 18:10:19 +0530

    add multiselect list in list view

--- lib/vachan_web/live/list_live/show.ex
+++ lib/vachan_web/live/list_live/show.ex
@@ -5,7 +5,9 @@ defmodule VachanWeb.ListLive.Show do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok,
+    socket
+    |> assign(:modal_visible, false)}
   end
 
   @impl true
@@ -14,6 +16,7 @@ defmodule VachanWeb.ListLive.Show do
       VachanWeb.Endpoint.subscribe("person_list:destroyed")
     end
 
+    mylists = Crm.List.read_all!(ash_opts(socket))
     list = Crm.List.get_by_id!(id, ash_opts(socket))
     person_details = get_person_details_for_list(list, socket)
 
@@ -21,6 +24,8 @@ defmodule VachanWeb.ListLive.Show do
      socket
      |> assign(:page_title, page_title(socket.assigns.live_action))
      |> assign(:person_details, person_details)
+     |> assign(:mylists, mylists)
+     |> assign(:get_list, [])
      |> assign(:list, Crm.List.get_by_id!(id, ash_opts(socket)))}
   end
 
@@ -29,6 +34,38 @@ defmodule VachanWeb.ListLive.Show do
     list = Crm.List.get_by_id!(id, ash_opts(socket))
     {:ok, _} = Crm.List.remove_person(list, person_id, ash_opts(socket))
     {:noreply, socket}
+  end
+
+
+   def handle_event("show_modal", %{"person_id" => person_id, "person_name" => person_name}, socket) do
+    person = Crm.Person.get_by_id!(person_id, ash_opts(socket))
+    get_list = get_list_person(person, socket)
+    IO.inspect(get_list, label: "person_id")
+    # {:noreply, assign(socket, :modal_visible, true)}
+    {:noreply,
+    socket
+    |> assign(:modal_visible, true)
+    |> assign(:person_id, person_id)
+    |> assign(:person_name, person_name)
+    |> assign(:get_list, get_list)}
+  end
+
+  @impl true
+  def handle_event("add_list", %{"person_id" => person_id, "list_id" => list_id}, socket) do
+    list = Crm.List.get_by_id!(list_id, ash_opts(socket))
+    {:ok, _} = Crm.List.add_person(list, person_id, ash_opts(socket))
+    {:noreply, socket}
+  end
+
+  defp get_list_person(person, socket) do
+    person
+    |> Ash.load!(:lists, ash_opts(socket))
+    |> then(fn x -> x.lists end)
+    |> Enum.map(fn x -> x.id end)
+  end
+
+  def handle_event("hide_modal", _params, socket) do
+    {:noreply, assign(socket, :modal_visible, false)}
   end
 
   @impl true
